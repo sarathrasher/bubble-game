a5c7426c745b726f2012d31ab7704089c6ca4608
Sara Muntean <saramuntean@Saras-MacBook-Pro.local>
2018-07-10 15:03:46 -0400

    Started over making the entire program with OOP

--- bubblegame.py
+++ bubblegame.py
@@ -2,39 +2,38 @@ import pygame, sys
 from pygame.locals import *
 from random import randint
 
-clock = pygame.time.Clock()
-
 WHITE = (255, 255, 255)
 
-width = 800
-height = 600
-
-# class Bubble(pygame.sprite.Sprite):
-#     def __init__(self, x, y):
- 
-    
-#     def disappear(self):
-#         pass
-
-#     def collision(self):
-#         pass
-#         # if bubble hits edge of screen/another bubble, reverse velocity
-
-
-
 class Player(pygame.sprite.Sprite):
-    # if arrow key pressed, move in that direction for as long as the key is pressed
-    # if hit wall, do not move further
     def __init__(self, x, y):
         super(Player, self).__init__()
-        self.score = 0
-        self.x = x
-        self.y = y
-        self.change_x = 0
-        self.change_y = 0
         self.image = pygame.image.load('yellow.png').convert_alpha()
         self.rect = self.image.get_rect()
-        
+        self.change_x = 0
+        self.change_y = 0
+        self.rect.x = x
+        self.rect.y = y
+
+    def handle_input(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.change_speed(-3, 0)
+            elif event.key == pygame.K_RIGHT:
+                self.change_speed(3, 0)
+            elif event.key == pygame.K_UP:
+                self.change_speed(0, -3)
+            elif event.key == pygame.K_DOWN:
+                self.change_speed(0, 3)
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                self.change_speed(3, 0)
+            elif event.key == pygame.K_RIGHT:
+                self.change_speed(-3, 0)
+            elif event.key == pygame.K_UP:
+                self.change_speed(0, 3)
+            elif event.key == pygame.K_DOWN:
+                self.change_speed(0, -3)
+    
     def change_speed(self, x, y):
         self.change_x += x
         self.change_y += y
@@ -46,90 +45,48 @@ class Player(pygame.sprite.Sprite):
         #Check collision with wall
         if self.rect.left <= 0:
             self.rect.left = 0
-        elif self.rect.right >= width:
-            self.rect.right = width
+        elif self.rect.right >= game.width:
+            self.rect.right = game.width
         if self.rect.top <= 0:
             self.rect.top = 0
-        elif self.rect.bottom >= height:
-            self.rect.bottom = height
+        elif self.rect.bottom >= game.height:
+            self.rect.bottom = game.height
 
-    def earn_point(self):
-        # if player hits up_point, add point
-        self.score += 1
-        
-    def lose_point(self):
-        #if player collides with down_point, lose point
-        self.score -= 1
+class Run_game(object):
+    def main(self):
+        # declare the size of the canvas
+        self.width = 800
+        self.height = 600
+        display_color = WHITE
+        pygame.init()
+        screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption('Bubblemania')
 
-# class Up_point(Bubble):
-#     pass
-#     #appear at random place on screen
-#     #disappear when player collides with it
-#     def __init(self):
-#         self.image = pygame.image.load('green.png').convert_alpha()
+        # Initialize game
+        self.player = Player(50, 50)
+        all_sprites_list = pygame.sprite.Group()
+        all_sprites_list.add(self.player)
+        clock = pygame.time.Clock()
+        done = False
 
-# class Down_point(Bubble):
-#     pass
-#     #speed = [(randint, randint)]
-#     #if collision with player bubble, decrease point by 1 and disappear
-#     #if no points, game over
-#     #every 30 seconds, add new bubble
-#     #every 30 sec, increase velocity of bubble
-#     def __init__(self):
-#         self.image = pygame.image.load('red.png').convert_alpha()
+        while True:
+            # Main game loop
 
-def main():
-    # declare the size of the canvas
-    width = 800
-    height = 600
-    display_color = (255, 255, 255)
-    pygame.init()
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('Bubblemania')
-    # Initialize game
-    player = Player(50, 50)
-    all_sprites_list = pygame.sprite.Group()
-    all_sprites_list.add(player)
-    clock = pygame.time.Clock()
-    done = False
-    while True:
-        # Main game loop
+            #Event Handling
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if pygame.key.get_pressed():  
+                    self.player.handle_input(event)
 
-        #Event Handling
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    player.change_speed(-3, 0)
-                elif event.key == pygame.K_RIGHT:
-                    player.change_speed(3, 0)
-                elif event.key == pygame.K_UP:
-                    player.change_speed(0, -3)
-                elif event.key == pygame.K_DOWN:
-                    player.change_speed(0, 3)
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
-                    player.change_speed(3, 0)
-                elif event.key == pygame.K_RIGHT:
-                    player.change_speed(-3, 0)
-                elif event.key == pygame.K_UP:
-                    player.change_speed(0, 3)
-                elif event.key == pygame.K_DOWN:
-                    player.change_speed(0, -3)
+            #Game display
+            all_sprites_list.update()
+            screen.fill(WHITE)
+            all_sprites_list.draw(screen)
+            pygame.display.update()
+            clock.tick(60)
+    pygame.quit()
 
-        #Game Logic
-            #start with just player bubble
-            #add up_point bubble, score += 1 when collision
-            #after score > 3, add down_point bubble
-            #after X amount of time, increase speed of down_point bubble
-        all_sprites_list.update()
-        #Game display
-        screen.fill(WHITE)
-        all_sprites_list.draw(screen)
-        pygame.display.update()
-        clock.tick(60)
-pygame.quit()
-main()
-        
+game = Run_game()
+game.main()
