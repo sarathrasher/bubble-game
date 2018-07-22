d8f4c9f217937b093fd6a2000114a6f3d59bf4e3
Sara Muntean <saramuntean@Saras-MacBook-Pro.local>
2018-07-22 12:06:16 -0400

    Test commit

--- bubblegame.py
+++ bubblegame.py
@@ -2,10 +2,11 @@ import pygame, sys
 from pygame.locals import *
 import random
 
-
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super(Player, self).__init__()
+        self.width = 800
+        self.height = 600
         self.image = pygame.image.load('yellow.png').convert_alpha()
         self.rect = self.image.get_rect()
         self.change_x = 0
@@ -15,6 +16,7 @@ class Player(pygame.sprite.Sprite):
         self.score = 0
         self.points = ()
         self.attack_bubbles = ()
+        self.prizes = ()
 
     def handle_input(self, event):
         if event.type == pygame.KEYDOWN:
@@ -47,12 +49,12 @@ class Player(pygame.sprite.Sprite):
         #Check collision with wall
         if self.rect.left <= 0:
             self.rect.left = 0
-        elif self.rect.right >= game.width:
-            self.rect.right = game.width
+        elif self.rect.right >= self.width:
+            self.rect.right = self.width
         if self.rect.top <= 0:
             self.rect.top = 0
-        elif self.rect.bottom >= game.height:
-            self.rect.bottom = game.height
+        elif self.rect.bottom >= self.height:
+            self.rect.bottom = self.height
 
     def check_collision(self, group):
         collide = pygame.sprite.spritecollide(self, group, True)
@@ -61,7 +63,6 @@ class Player(pygame.sprite.Sprite):
                 self.add_score()
             elif group == self.attack_bubbles:
                 self.subtract_score()
-            print self.score
 
     def add_score(self):
         self.score += 1
@@ -74,29 +75,44 @@ class Point(pygame.sprite.Sprite):
         super(Point, self).__init__()
         self.image = pygame.image.load('green.png').convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.x = random.randint((0 + self.rect.width), (game.width - self.rect.width))
-        self.rect.y = random.randint((0 + self.rect.height), (game.height - self.rect.height))
+        self.game = App()
+        width = self.game.width
+        height = self.game.height
+        self.rect.x = random.randint((0 + self.rect.width), (width - self.rect.width))
+        self.rect.y = random.randint((0 + self.rect.height), (height - self.rect.height))
+
+class Prize(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Prize, self).__init__()
+        self.image = pygame.image.load("turkish.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint((0 + self.rect.width), (self.width - self.rect.width))
+        self.rect.y = random.randint((0 + self.rect.height), (self.height - self.rect.height))
+
 
 class Attack(pygame.sprite.Sprite):
     def __init__(self):
         super(Attack, self).__init__()
+        self.game = App()
+        self.width = self.game.width
+        self.height = self.game.height
         self.image = pygame.image.load('red.png').convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.x = random.randint((0 + self.rect.width), (game.width - (self.rect.width)))
-        self.rect.y = random.randint((0 + self.rect.height), (game.height - self.rect.height))
-        self.speed_x = random.randint(3, 5)
-        self.speed_y = random.randint(3, 5)
+        self.rect.x = random.randint((0 + self.rect.width), (self.width - (self.rect.width)))
+        self.rect.y = random.randint((0 + self.rect.height), (self.height - self.rect.height))
+        self.speed_x = random.randint(4, 6)
+        self.speed_y = random.randint(4, 6)
     
     def update(self):
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
 
         #Check collision with wall
-        if (self.rect.left + self.speed_x) <= 0 or (self.rect.right + self.speed_x) >= game.width:
+        if (self.rect.left + self.speed_x) <= 0 or (self.rect.right + self.speed_x) >= self.width:
             self.speed_x = -self.speed_x
-        if (self.rect.top + self.speed_y) <= 0 or (self.rect.bottom + self.speed_y) >= game.height:
+        if (self.rect.top + self.speed_y) <= 0 or (self.rect.bottom + self.speed_y) >= self.height:
             self.speed_y = -self.speed_y
-
+    
 def draw_text(screen, text, size, x, y):
     font_name = pygame.font.match_font("arial")
     BLACK = (0, 0, 0)
@@ -106,85 +122,105 @@ def draw_text(screen, text, size, x, y):
     text_rect.midtop = (x, y)
     screen.blit(text_surface, text_rect)
 
-def show_title_screen():
-    WHITE = (255, 255, 255)
-    screen = game.screen
-    width = game.width
-    height = game.height
-    screen.fill(WHITE)
-    draw_text(screen, "BUBBLEMANIA", 70, width / 2, height / 4)
-    draw_text(screen, "Collect the green bubbles while avoiding red bubbles.", 30, width / 2, height / 3)
-    draw_text(screen, "Press any key to begin.", 30, width / 2, height / 2)
-    pygame.display.update()
-    waiting = True
-    while waiting:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-            if event.type == pygame.KEYUP:
-                waiting = False
-
+class Show_title(object):
+    def show_title_screen(self, screen, app):
+        WHITE = (255, 255, 255)
+        self.game = App()
+        width = app.width
+        height = app.height
+        screen.fill(WHITE)
+        draw_text(screen, "BUBBLEMANIA", 70, width / 2, height / 4)
+        draw_text(screen, "Collect the green bubbles while avoiding red bubbles.", 30, width / 2, height / 3)
+        draw_text(screen, "Press any key to begin.", 30, width / 2, height / 2)
+        pygame.display.update()
+        game_over = True
+        while game_over:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                if event.type == pygame.KEYUP:
+                    game_over = False
 
 class Run_game(object):
-    def main(self):
-        # declare the size of the canvas
-        self.width = 800
-        self.height = 600
-        WHITE = (255, 255, 255)
-        display_color = WHITE
-        pygame.init()
-        self.screen = pygame.display.set_mode((self.width, self.height))
-        pygame.display.set_caption('Bubblemania')
+    def __init__(self):
 
         # Initialize game
         self.player = Player(50, 50)
-        player_group = pygame.sprite.Group()
-        player_group.add(self.player)
-        all_sprites_list = pygame.sprite.Group()
+        self.player_group = pygame.sprite.Group()
+        self.player_group.add(self.player)
+        self.all_sprites_list = pygame.sprite.Group()
         self.player.points = pygame.sprite.Group()
         self.player.attack_bubbles = pygame.sprite.Group()
-        clock = pygame.time.Clock()
-        done = False
-        game_over = True
+        self.player.prizes = pygame.sprite.Group()
+        self.clock = pygame.time.Clock()
+        self.running = True
 
-        while True:
+    def game_loop(self, screen, app):
+        while self.running:
             # Main game loop
-            if game_over:
-                show_title_screen()
-                game_over = False
-            while len(self.player.points) <= 3:
+            # if game_over:
+            #     show_title_screen()
+            #     game_over = False
+            while len(self.player.points) < 3:
                 self.point = Point()
-                all_sprites_list.add(self.point)
+                self.all_sprites_list.add(self.point)
                 self.player.points.add(self.point)
-            while len(self.player.attack_bubbles) <= 2:
+            while len(self.player.attack_bubbles) < 3:
                 self.attack = Attack()
-                all_sprites_list.add(self.attack)
+                self.all_sprites_list.add(self.attack)
                 self.player.attack_bubbles.add(self.attack)
-                
-                if self.player.score < 0:
-                    game_over = True
 
-            #Event Handling
+            if self.player.score < 0:
+                game_over = True
+                self.running = False
+
+            # Event Handling
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
                 if pygame.key.get_pressed():  
                     self.player.handle_input(event)
+            
 
             #Game display
-            player_group.update()
-            all_sprites_list.update()
+            self.player_group.update()
+            self.all_sprites_list.update()
             self.player.check_collision(self.player.points)
             self.player.check_collision(self.player.attack_bubbles)
-            self.screen.fill(WHITE)
-            player_group.draw(self.screen)
-            all_sprites_list.draw(self.screen)
-            draw_text(self.screen, "Current score is %d" % self.player.score, 18, self.width / 2, 10)
+            self.player.check_collision(self.player.prizes)
+            screen.fill(app.WHITE)
+            self.player_group.draw(screen)
+            self.all_sprites_list.draw(screen)
+            draw_text(screen, "Current score is %d" % self.player.score, 18, app.width / 2, 10)
             pygame.display.update()
-            clock.tick(60)
+            self.clock.tick(60)
         
-    pygame.quit()
 
-game = Run_game()
-game.main()
+class App(object):
+    def __init__(self):
+        self.width = 800
+        self.height = 600
+        self.WHITE = (255, 255, 255)
+        display_color = self.WHITE
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption('Bubblemania')
+        self.title = Show_title()
+        self.game = Run_game()
+
+    def run(self):
+        self.running = True
+        while self.running:
+            game_over = True
+            while game_over:
+                self.title.show_title_screen(self.screen, self)
+                game_over = False
+            while game_over == False:
+                self.game.game_loop(self.screen, self)
+                game_over = True
+        pygame.quit()
+
+
+go = App()
+go.run()
